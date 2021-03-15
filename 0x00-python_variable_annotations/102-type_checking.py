114a034b6fae11d0986dd9297cbf41c86485566e
tatsOre <tatianaorezap@gmail.com>
2021-03-15 18:49:57 -0500

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""""
+Module for 12. Type Checking.
+0x00. Python - Variable Annotations
+Holberton Web Stack programming Spec ― Back-end
+"""
+from typing import Tuple, List, cast
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Method that zooms an array"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(cast(Tuple, array))
+
+zoom_3x = zoom_array(cast(Tuple, array), cast(int, 3.0))
