250c1dc6ed445cb1f1c2fade886aee03baeb31a4
tatsOre <tatianaorezap@gmail.com>
2021-03-22 11:19:49 -0500

    LFU caching

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for 100.
+Module for 5. LFU Caching
 0x03. Caching
 Holberton Web Stack programming Spec ― Back-end
 """
@@ -17,32 +17,27 @@ class LFUCache(BaseCaching):
     def __init__(self):
         super().__init__()
         self.__queue = deque()
-        self.__items_count = {}
+        self.__count = {}
 
     def put(self, key, item):
         """
         Assigns to the dictionary `self.cache_data` the `item` value
         for the key `key`.
         """
+        size = len(self.cache_data)
         if key and item:
-            if key in self.cache_data:
-                self.__items_count[key] += 1
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if size >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
                 self.discard()
-
-            if key not in self.cache_data:
-                self.__items_count[key] = 1
-
             self.cache_data[key] = item
+            self.count(key)
 
     def get(self, key):
         """
         Returns the value in `self.cache_data` linked to `key`
         or `None` if the key doesn’t exist in `self.cache_data`.
-        Keeps track of used items.
         """
         if key in self.cache_data:
-            self.__items_count[key] += 1
+            self.__count[key] += 1
             return self.cache_data.get(key)
         return None
 
@@ -52,14 +47,23 @@ class LFUCache(BaseCaching):
         """
         removed = self.least_frequent()
         del self.cache_data[removed]
-        del self.__items_count[removed]
+        del self.__count[removed]
         print(f"DISCARD: {removed}")
 
     def least_frequent(self):
         """
         Returns the least frequent used item
         """
-        LFU = min([value for value in self.__items_count.values()])
-        for key in self.__items_count.keys():
-            if self.__items_count[key] == LFU:
+        LFU = min([value for value in self.__count.values()])
+        for key in self.__count.keys():
+            if self.__count[key] == LFU:
                 return key
+
+    def count(self, key):
+        """
+        Keeps track of the frequency of use of the items.
+        """
+        if key in self.__count:
+            self.__count[key] += 1
+        else:
+            self.__count[key] = 1
