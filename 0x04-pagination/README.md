ad08c05a6884734c5d3faa8f7afe53e8fc30a366
tatsOre <tatianaorezap@gmail.com>
2021-03-24 08:46:10 -0500

    Pagination - first commit

---
+++ 0x04-pagination/README.md
@@ -0,0 +1,14 @@
+# 0x04. Pagination
+
+### Learning Objectives:
+* How to paginate a dataset with simple page and page_size parameters
+* How to paginate a dataset with hypermedia metadata
+* How to paginate in a deletion-resilient manner
+
+### Resources:
+
+### Author:
+* Tatiana Orejuela Zapata | [Github](https://github.com/tatsOre)
+
+##### Specializations - Web Stack programming ― Back-end
+##### March, 2020. Cali, Colombia.
---
+++ 0x04-pagination/download_save_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+0x04-pagination
+Holberton Web Stack programming Spec ― Back-end
+"""
+import requests
+
+URL = 'https://holbertonintranet.s3.amazonaws.com/uploads/misc/2020/5/7d3576d97e7560ae85135cc214ffe2b3412c51d7.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARDDGGGOUWMNL5ANN%2F20210323%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210323T153049Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=c5260561b2d189b5f12e07c9335c378aa2048e69c515803e57c78ed20e894b32'
+
+DATA_FILE = "Popular_Baby_Names.csv"
+
+
+def download_csv(URL: str, FILENAME: str):
+    """
+    Helper method that downloads csv data from URL
+    and saves content in to a file
+    """
+    req = requests.get(URL)
+    content = req.content
+    with open(DATA_FILE, 'wb') as csv_file:
+        csv_file.write(content)
+
+
+download_csv(URL, FILENAME)
