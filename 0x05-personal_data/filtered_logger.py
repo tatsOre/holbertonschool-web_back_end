1bfb769d8f159af10f5e226f7a21b94f2e86759c
tatsOre <tatianaorezap@gmail.com>
2021-03-31 07:09:44 -0500

    --Really?

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -21,7 +21,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]:
+    def __init__(self, fields: List[str]):
         """ Constructor """
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
