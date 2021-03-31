f0db267ca8d1f84d0a581d8a6ab70a33b458c781
tatsOre <tatianaorezap@gmail.com>
2021-03-31 07:02:55 -0500

    Missing Type Anno

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ import re
 import logging
 import os
 import mysql.connector
-from typing import List
+from typing import List, Tuple
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -21,14 +21,14 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: Tuple[str]:
         """ Constructor """
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
         """
-        Method that filters values in incoming log records using `filter_datum`
+        Filters values in incoming log records using `filter_datum`
         """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
