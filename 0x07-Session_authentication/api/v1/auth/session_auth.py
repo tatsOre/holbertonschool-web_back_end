94e9560e5c794d9e9529d31fd7dd71ade1c23890
tatsOre <tatianaorezap@gmail.com>
2021-04-06 14:18:54 -0500

    Empty Session

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module for BasicAuth class to manage the API authentication.
+"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """ Defines the SessionAuth class that inherits from Auth """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Documentation for a method"""
+        if not user_id or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Documentation for a method"""
+        if not session_id or type(session_id) != str:
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Documentation for a method"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
