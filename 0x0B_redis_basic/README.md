4e007a04e1b66f9848fd506c5675f88a42333657
tatsOre <tatianaorezap@gmail.com>
2021-04-27 10:15:57 -0500

    Redis basic

---
+++ 0x0B_redis_basic/README.md
@@ -0,0 +1,14 @@
+# 0x0B. Redis basic
+
+### Learning Objectives:
+* Learn how to use redis for basic operations
+* Learn how to use redis as a simple cache
+
+### Resources:
+* [How to Use Redis With Python](https://realpython.com/python-redis/)
+
+### Author:
+* Tatiana Orejuela Zapata | [Github](https://github.com/tatsOre)
+
+##### Specializations - Web Stack programming ― Back-end
+##### April, 2020.
---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+Module for exercise.py
+0x0B_redis_basic
+Holberton Web Stack programming Spec ― Back-end
+"""
+from functools import wraps
+import redis
+from typing import Union, Callable, Optional
+import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Counts how many times methods of the Cache class are called
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Increments the count for a key every time the method is called
+        Return:
+            Value returned by the original method
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Stores the history of inputs and outputs for a called function
+    """
+    @wraps(method)
+    def wrapper(self, *args):
+        """
+        Adds method's input parameters to one list in redis,
+        and store its output into another list
+        """
+        self._redis.rpush(f"{method.__qualname__}:inputs", str(args))
+        output = method(self, *args)
+        self._redis.rpush(f"{method.__qualname__}:outputs", str(output))
+        return output
+    return wrapper
+
+
+class Cache:
+    """ Defines the class Cache """
+    def __init__(self):
+        """ Constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Generates a random key using uuid and stores the input data in Redis
+        Return:
+            Random generated key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """
+        Gets a value from the redis data instance and converts it back
+        to the desired format using the passed Callable `fn`
+        Return:
+            Data in its original type/format
+        """
+        data = self._redis.get(key)
+        if data and fn:
+            return fn(data)
+        return data
