e832298ca1e8f1e08553264b1b79e9a5fa4516f2
tatsOre <tatianaorezap@gmail.com>
2021-04-29 11:29:06 -0500

    Create triggers

---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- 4-store.sql / 0x0C. MySQL advanced /  Web Stack programming ― Back-end
+-- Creates a trigger that decreases the quantity of an item after adding a new order
+DELIMITER $$
+CREATE
+    TRIGGER `decreases_items_quantity` AFTER INSERT ON orders
+    FOR EACH ROW
+        BEGIN
+        UPDATE items
+            SET quantity = quantity - NEW.number
+            WHERE name = NEW.item_name;
+        END;
+$$
+DELIMITER ;
