0d3d71a4f80a7838dc19f93dfbc3516d3604c487
tatsOre <tatianaorezap@gmail.com>
2021-04-29 15:19:53 -0500

    Email validation to sent

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- 5-valid_email.sql / 0x0C. MySQL advanced /  Web Stack programming ― Back-end
+--  Creates a trigger that resets the attribute valid_email only when the email has been changed
+DELIMITER $$
+CREATE
+    TRIGGER `email_validation`
+    BEFORE UPDATE ON `users`
+    FOR EACH ROW
+    BEGIN
+        IF NEW.email <> OLD.email
+            THEN
+                SET NEW.valid_email = !OLD.valid_email;
+        END IF;
+    END$$
+DELIMITER ;
