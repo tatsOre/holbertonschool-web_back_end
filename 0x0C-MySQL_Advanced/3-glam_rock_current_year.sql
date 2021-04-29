38f6605b0bb49d55438949cd45c2feff35341cb9
tatsOre <tatianaorezap@gmail.com>
2021-04-29 09:45:03 -0500

    Changing year for 2020...

--- 0x0C-MySQL_Advanced/3-glam_rock_current_year.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock_current_year.sql
@@ -1,6 +1,6 @@
 -- 3-glam_rock.sql / 0x0C. MySQL advanced /  Web Stack programming ― Back-end
 -- Lists all bands with `Glam rock` as their main style, ranked by their longevity
-SELECT `band_name`, IFNULL(`split`, 2020) - `formed` AS `lifespan`
+SELECT `band_name`, IFNULL(`split`, YEAR(CURDATE())) - `formed` AS `lifespan`
 FROM `metal_bands`
 WHERE `style` LIKE '%Glam rock%'
 ORDER BY `lifespan` DESC;
