6c71b4e41f190663f9e6ab034f03f0cd5a9e5f9d
tatsOre <tatianaorezap@gmail.com>
2021-05-04 16:36:03 -0500

    NoSQL - Mongo - PyMongo Tasks

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -8,5 +8,8 @@ import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
     return mongo_collection.update_many({"name": name},
                                         {"$set": {"topics": topics}})
