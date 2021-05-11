390d4b0e86119d5b3fba8d6ee4358881e12707f1
tatsOre <tatianaorezap@gmail.com>
2021-05-11 08:47:28 -0500

    [Upload] Promises mandatory tasks

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      const photoName = result[0].body;
+      const { firstName, lastName } = result[1];
+      console.log(`${photoName} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
