3f3f584c176bc972f1ba89aca718684a15ec7222
tatsOre <tatianaorezap@gmail.com>
2021-05-11 09:25:44 -0500

    Handle multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,5 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((result) => {
+      for (const promise of result) {
+        if (promise.status === 'rejected') {
+          promise.value = `Error: ${promise.reason.message}`;
+          delete promise.reason;
+        }
+      }
+      return result;
+    });
 }
