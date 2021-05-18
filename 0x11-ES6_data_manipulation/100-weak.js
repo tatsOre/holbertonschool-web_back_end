f8485a3c0d2130a024ac5b9fd89b5c746641c26b
tatsOre <tatianaorezap@gmail.com>
2021-05-18 10:24:27 -0500

    [Upload] - ES6 Data Manipulation

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -2,6 +2,8 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
-  if (count >= 5) throw new Error('Endpoint load is high');
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
   weakMap.set(endpoint, count + 1);
 }
