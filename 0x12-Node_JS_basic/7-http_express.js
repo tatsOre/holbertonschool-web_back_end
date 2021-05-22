d30aa4548a175cbb13166b7bec02d7de1270b0e9
tatsOre <tatianaorezap@gmail.com>
2021-05-22 11:37:48 -0500

    Express small HTTP Server

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const { totalMsg, csMsg, sweMsg } = await countStudents(database);
+  const response = `${totalMsg}\n${csMsg}\n${sweMsg}`;
+  res.send(response);
+});
+
+app.listen(port);
+
+export default app;
